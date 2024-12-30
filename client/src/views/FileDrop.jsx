d5fe9f086b02ff7c713ed3c57a06fff89d271d48
Himanshu090705 <himanshujagtap369@gmail.com>
2024-12-30 19:39:04 +0530

    Removed unwanted console.log

--- client/src/views/FileDrop.jsx
+++ client/src/views/FileDrop.jsx
@@ -4,27 +4,28 @@ import useFileStore from "../store/fileStore";
 import "./FileDrop.css";
 
 function FileDrop() {
-  const setFileData = useFileStore((state) => state.setFileData);
-  const onDrop = useCallback(
-    (acceptedFiles) => {
-      acceptedFiles.forEach((file) => {
-        console.log(file);
-        setFileData(file);
-      });
-    },
-    [setFileData],
-  );
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+    const setFileData = useFileStore((state) => state.setFileData);
+    const onDrop = useCallback(
+        (acceptedFiles) => {
+            acceptedFiles.forEach((file) => {
+                setFileData(file);
+            });
+        },
+        [setFileData]
+    );
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+    });
 
-  return (
-    <div {...getRootProps()} className="drop-container">
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag n drop some files here, or click to select files</p>
-      )}
-    </div>
-  );
+    return (
+        <div {...getRootProps()} className="drop-container">
+            <input {...getInputProps()} />
+            {isDragActive ? (
+                <p>Drop the files here ...</p>
+            ) : (
+                <p>Drag n drop some files here, or click to select files</p>
+            )}
+        </div>
+    );
 }
 export default FileDrop;
