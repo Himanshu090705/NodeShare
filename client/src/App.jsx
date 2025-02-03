a76d9dbe94e79b4841f3188dda3654e676d2c72c
Himanshu090705 <himanshujagtap369@gmail.com>
2025-02-03 23:27:07 +0530

    added login & signup component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
-import Upload from './views/Upload';
-import Download  from './views/Download';
-import Navbar from './views/Navbar';
+import { Routes, Route } from "react-router-dom";
+import Upload from "./views/Upload";
+import Download from "./views/Download";
+import Navbar from "./views/Navbar";
+import Footer from "./views/Footer";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Upload />} />
-        <Route path="/file/:id" element={<Download />} />
-      </Routes>
-      <Footer/>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Upload />} />
+                <Route path="/file/:id" element={<Download />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- client/src/views/Footer.jsx
+++ client/src/views/Footer.jsx
@@ -2,34 +2,67 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-light py-4 mt-5">
+        <footer className="bg-dark py-3 mt-3">
             <div className="container text-center">
+                <hr className="mt-4" />
                 <div className="mb-3">
-                    <a className="mx-2 text-dark" href="https://www.facebook.com/">
+                    <a
+                        className="mx-2 text-light"
+                        href="https://www.facebook.com/"
+                    >
                         <i className="fa-brands fa-square-facebook fa-2x"></i>
                     </a>
-                    <a className="mx-2 text-dark" href="https://www.instagram.com/">
+                    <a
+                        className="mx-2 text-light"
+                        href="https://www.instagram.com/"
+                    >
                         <i className="fa-brands fa-square-instagram fa-2x"></i>
                     </a>
-                    <a className="mx-2 text-dark" href="https://www.linkedin.com/feed/">
+                    <a
+                        className="mx-2 text-light"
+                        href="https://www.linkedin.com/feed/"
+                    >
                         <i className="fa-brands fa-linkedin fa-2x"></i>
                     </a>
-                    <a className="mx-2 text-dark" href="https://x.com/">
+                    <a className="mx-2 text-light" href="https://x.com/">
                         <i className="fa-brands fa-twitter fa-2x"></i>
+                    </a>
+                    <a className="mx-2 text-light" href="https://github.com/">
+                        <i className="fa-brands fa-github fa-2x"></i>
                     </a>
                 </div>
                 <div className="mb-3">
-                    <a className="text-decoration-none text-dark mx-2" href="/listings/privacy">Privacy</a>
-                    <a className="text-decoration-none text-dark mx-2" href="/listings/terms">Terms</a>
-                    <a className="text-decoration-none text-dark mx-2" href="/listings/contact">Contact Us</a>
-                    <a className="text-decoration-none text-dark mx-2" href="/listings/about">About Us</a>
+                    <a
+                        className="text-decoration-none text-light mx-2"
+                        href="/listings/privacy"
+                    >
+                        Privacy
+                    </a>
+                    <a
+                        className="text-decoration-none text-light mx-2"
+                        href="/listings/terms"
+                    >
+                        Terms
+                    </a>
+                    <a
+                        className="text-decoration-none text-light mx-2"
+                        href="/listings/contact"
+                    >
+                        Contact Us
+                    </a>
+                    <a
+                        className="text-decoration-none text-light mx-2"
+                        href="/listings/about"
+                    >
+                        About Us
+                    </a>
                 </div>
                 <div className="text-muted">
-                    WanderLust Private Limited &nbsp;
-                    <i className="fa-regular fa-copyright"></i> 2024 - All Rights Reserved &nbsp;
+                    NodeShare Private Limited &nbsp;
+                    <i className="fa-regular fa-copyright"></i> 2025 - All
+                    Rights Reserved &nbsp;
                     <i className="fa-solid fa-registered"></i>
                 </div>
-                <hr className="mt-4" />
             </div>
         </footer>
     );
