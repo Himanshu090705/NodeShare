9434f03f214cdff9e5d0ba061486f0318891fd0b
Himanshu090705 <himanshujagtap369@gmail.com>
2025-04-02 08:56:08 +0530

    home style add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,23 +6,24 @@ import Footer from "./views/Footer";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Home from "./views/Home";
+import "./index.css";
 
 function App() {
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <main className="flex-grow-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/file/:id" element={<Download />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Navbar />
+            <main className="flex-grow-1">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/upload" element={<Upload />} />
+                    <Route path="/file/:id" element={<Download />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- client/src/views/Navbar.jsx
+++ client/src/views/Navbar.jsx
@@ -1,75 +1,88 @@
 import { NavLink } from "react-router-dom";
 import useUserStore from "../store/userStore";
+
 function Navbar() {
-  const isLogin = useUserStore((state) => state.isLogin);
-  const setIsLogin = useUserStore((state) => state.setIsLogin);
-  return (
-    <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          <i className="fa-solid fa-retweet"></i> NodeShare
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              {isLogin ? (
-                <NavLink className="nav-link" aria-current="page" to="/upload">
-                  <i className="fa-solid fa-share-nodes"></i> Share
-                </NavLink>
-              ) : (
-                <NavLink className="nav-link" aria-current="page" to="/login">
-                  <i className="fa-solid fa-share-nodes"></i> Share
-                </NavLink>
-              )}
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="#">
-                <i className="fa-solid fa-arrows-turn-to-dots"></i> Convert
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="#">
-                <i className="fa-solid fa-minimize"></i> Compress
-              </NavLink>
-            </li>
-          </ul>
-          <div className="navbar-nav ms-auto">
-            {!isLogin ? (
-              <>
-                <NavLink className="nav-link" to="/signup">
-                  <i className="fa-solid fa-user-plus"></i>&nbsp;
-                  <b>Sign Up</b>
-                </NavLink>
-                <NavLink className="nav-link" to="/login">
-                  <i className="fa-solid fa-right-to-bracket"></i>&nbsp;
-                  <b>Login</b>
-                </NavLink>
-              </>
-            ) : (
-              <NavLink
-                className="btn btn-light"
-                onClick={() => setIsLogin(false)}
-                to={"/"}
-              >
-                <i className="fa-solid fa-right-to-bracket"></i>&nbsp;
-                <b>Logout</b>
-              </NavLink>
-            )}
-          </div>
+    const isLogin = useUserStore((state) => state.isLogin);
+    const setIsLogin = useUserStore((state) => state.setIsLogin);
+
+    return (
+        <div className="">
+            <nav className="navbar navbar-expand-lg fixed-top navbar-box shadow-lg">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand text-white" to="/">
+                        <i className="fa-solid fa-retweet"></i> NodeShare
+                    </NavLink>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                {isLogin ? (
+                                    <NavLink
+                                        className="nav-link text-white"
+                                        to="/upload"
+                                    >
+                                        <i className="fa-solid fa-share-nodes"></i>{" "}
+                                        Share
+                                    </NavLink>
+                                ) : (
+                                    <NavLink
+                                        className="nav-link text-white"
+                                        to="/login"
+                                    >
+                                        <i className="fa-solid fa-share-nodes"></i>{" "}
+                                        Share
+                                    </NavLink>
+                                )}
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-white" to="#">
+                                    <i className="fa-solid fa-arrows-turn-to-dots"></i>{" "}
+                                    Convert
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link text-white" to="#">
+                                    <i className="fa-solid fa-minimize"></i>{" "}
+                                    Compress
+                                </NavLink>
+                            </li>
+                        </ul>
+                        <div className="navbar-nav ms-auto">
+                            {!isLogin ? (
+                                <>
+                                    <NavLink
+                                        className="nav-link text-white"
+                                        to="/signup"
+                                    >
+                                        <i className="fa-solid fa-user-plus"></i>
+                                        &nbsp;
+                                        <b>Sign Up</b>
+                                    </NavLink>
+                                    <NavLink
+                                        className="nav-link text-white"
+                                        to="/login"
+                                    >
+                                        <i className="fa-solid fa-right-to-bracket"></i>
+                                        &nbsp;
+                                        <b>Login</b>
+                                    </NavLink>
+                                </>
+                            ) : (
+                                <NavLink
+                                    className="btn btn-light"
+                                    onClick={() => setIsLogin(false)}
+                                    to={"/"}
+                                >
+                                    <i className="fa-solid fa-right-to-bracket"></i>
+                                    &nbsp;
+                                    <b>Logout</b>
+                                </NavLink>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </div>
-      </div>
-    </nav>
-  );
+    );
 }
+
 export default Navbar;
