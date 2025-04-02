44611ec2f3e4db0211e84a2269427e76ab10e4c3
Himanshu090705 <himanshujagtap369@gmail.com>
2025-04-02 09:28:46 +0530

    Resolved commits

--- client/src/store/userStore.js
+++ client/src/store/userStore.js
@@ -1,13 +1,8 @@
 import { create } from "zustand";
 
 const useUserStore = create((set) => ({
-<<<<<<< HEAD
-  isLogin: false,
-  setIsLogin: (isLogin) => set({ isLogin }),
-=======
-  session: {},
-  setAuthSession: (session) => set({ session })
->>>>>>> 28e6fdf48fdfdb7390e0687af8e65b096a5830eb
+    session: {},
+    setAuthSession: (session) => set({ session }),
 }));
 
 export default useUserStore;
--- server/db/connect.js
+++ server/db/connect.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import mongoose from 'mongoose';
-
-const db = process.env.DB || "mongodb://localhost:27018/NodeShare"
-mongoose.connect(db).then(() => {
-    console.log("Database connected successfully");
-}).catch((error) => {
-    console.log('Unable to connect to database due to ', error);
-})
-=======
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_KEY, SUPABASE_URL } from "../../config.js";
 const url = SUPABASE_URL;
 const anonKey = SUPABASE_KEY;
 
 export const supabase = createClient(url, anonKey);
->>>>>>> 28e6fdf48fdfdb7390e0687af8e65b096a5830eb
